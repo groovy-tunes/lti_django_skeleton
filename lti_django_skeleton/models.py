b2bb541eb95b48e782a7ec2117778b186dc7ffc7
groovy-tunes <chinsenjeremy@gmail.com>
2017-07-13 09:47:48 -0400

    Fixed up models in lti_django_skeleton/models.py

--- lti_django_skeleton/models.py
+++ lti_django_skeleton/models.py
@@ -9,11 +9,25 @@ from django.db import models
 
 from ltilaunch.models import LTIUser
 
-class Course(models.Model):
-    name = CharField(max_length=255)
-    owner_id = ForeignKey(LTIUser, on_delete=models.CASCADE)
-    service = CharField(max_length=80, default="")
-    external_id = CharField(max_length=255, default="")
+
+class Base(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __tablename__(cls):
+        return cls.__name__.lower()
+    def __repr__(self):
+        return str(self)
+
+    class Meta:
+        abstract = True
+
+
+class Course(Base):
+    name = models.CharField(max_length=255)
+    owner_id = models.ForeignKey(LTIUser, on_delete=models.CASCADE)
+    service = models.CharField(max_length=80, default="")
+    external_id = models.CharField(max_length=255, default="")
 
     def __str__(self):
         return '<Course {}>'.format(self.id)
@@ -36,32 +50,231 @@ class Course(models.Model):
         else:
             return lti_course
 
-class Role(models.Model):
-    name = CharField(max_length=80)
-    user = ForeignKey(LTIUser, on_delete=models.CASCADE)
-    course = ForeignKey(Course, on_delete=models.CASCADE)
+
+class Role(Base):
+    name = models.CharField(max_length=80)
+    user = models.ForeignKey(LTIUser, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     NAMES = ['teacher', 'admin', 'student']
 
     def __str__(self):
         return '<User {} is {}>'.format(self.user, self.name)
 
-class Settings(models.Model):
-    mode = CharField(max_length=80)
-    connected = CharField(max_length=80)
-    user = ForeignKey(LTIUser, on_delete=models.CASCADE)
+
+class Settings(Base):
+    mode = models.CharField(max_length=80)
+    connected = models.CharField(max_length=80)
+    user = models.ForeignKey(LTIUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return '<{} settings ({})>'.format(self.user_id, self.id)
 
-class Submission(models.Model):
-    code = TextField(default="")
-    status = IntegerField(default=0)
-    correct = BooleanField(default=False)
-    assignment = ForeignKey(Assignment, on_delete=models.CASCADE)
-    user= ForeignKey(LTIUser, on_delete=models.CASCADE)
-    assignment_version = IntegerField(default=0)
-    version = IntegerField(default=0)
+
+class Assignment(Base):
+    url = models.CharField(max_length=255, default="")
+    name = models.CharField(max_length=255, default="Untitled")
+    body = models.TextField(default="")
+    on_run = models.TextField(default="def on_run(code, output, properties):\n    return True")
+    on_step = models.TextField(default="def on_step(code, output, properties):\n    return True")
+    on_start = models.TextField(default="")
+    answer = models.TextField(default="")
+    due = models.DateTimeField(default=None)
+    type = models.CharField(max_length=10, default="normal")
+    visibility = models.CharField(max_length=10, default="visible")
+    disabled = models.CharField(max_length=10, default="enabled")
+    mode = models.CharField(max_length=10, default="blocks")
+    owner = models.ForeignKey(LTIUser, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    version = models.IntegerField(default=0)
+
+    @staticmethod
+    def edit(assignment_id, presentation=None, name=None, on_run=None, on_step=None, on_start=None, parsons=None, text_first=None):
+        assignment = Assignment.objects.get(pk=assignment_id)
+        if name is not None:
+            assignment.name = name
+            assignment.version += 1
+        if presentation is not None:
+            assignment.body = presentation
+            assignment.version += 1
+        if on_run is not None:
+            assignment.on_run = on_run
+            assignment.version += 1
+        if on_step is not None:
+            assignment.on_step = on_step
+            assignment.version += 1
+        if on_start is not None:
+            assignment.on_start = on_start
+            assignment.version += 1
+        assignment.type = 'normal'
+        if parsons is True:
+            assignment.type = 'parsons'
+            assignment.version += 1
+        if text_first is True:
+            assignment.type = 'text'
+            assignment.version += 1
+        assignement.save()
+        return assignment
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+            'body': self.body,
+            'title': self.title()
+        }
+
+    def __str__(self):
+        return '<Assignment {} for {}>'.format(self.id, self.course.id)
+
+    def title(self):
+        return self.name if self.name != "Untitled" else "Untitled ({})".format(self.id)
+
+    @staticmethod
+    def new(owner_id, course_id):
+        owner = LTIUser.objects.get(pk=owner_id)
+        course = Course.objects.get(pk=course_id)
+        assignment = Assignment(owner=owner, course=course)
+        assignement.save()
+        return assignment
+
+    @staticmethod
+    def remove(assignment_id):
+        Assignment.objects.get(pk=assignment_id).delete()
+
+    @staticmethod
+    def by_course(course_id, exclude_builtins=True):
+        if exclude_builtins:
+            course = Course.objects.get(pk=course_id)
+            return (Assignment.objects.filter(course=course)
+						                        .exclude(mode=maze)
+                                    .all())
+        else:
+            return Assignment.objects.filter(course=course)
+
+    @staticmethod
+    def by_id(assignment_id):
+        return Assignment.objects.get(pk=assignment_id)
+
+    @staticmethod
+    def by_builtin(type, id, owner_id, course_id):
+        course = Course.object.get(pk=course_id)
+        assignment = Assignment.objects.filter(course=course,
+                                                mode=type,
+                                                name=id).first()
+        if not assignment:
+            assignment = Assignment.new(owner_id, course_id)
+            assignment.mode = type
+            assignment.name = id
+            assignment.save()
+        return assignment
+
+    @staticmethod
+    def by_id_or_new(assignment_id, owner_id, course_id):
+        if assignment_id is None:
+            assignment = None
+        else:
+            assignment = Assignment.objects.get(pk=assignment_id)
+        if not assignment:
+            assignment = Assignment.new(owner_id, course_id)
+        return assignment
+
+    def context_is_valid(self, context_id):
+        course = Course.objects.get(pk=self.course_id)
+        if course:
+            return course.external_id == context_id
+        return False
+
+    def get_submission(self, user_id):
+        return Submission.load(user_id, self.id)
+
+
+class AssignmentGroup(Base):
+    name = models.CharField(max_length=255, default="Untitled")
+    owner = models.ForeignKey(LTIUser, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    @staticmethod
+    def new(owner_id, course_id):
+        owner = LTIUser.objects.get(pk=owner_id)
+        course = Course.objects.get(pk=course_id)
+        assignment_group = AssignmentGroup(owner=owner, course=course)
+        assignment_group.save()
+        return assignment_group
+
+    @staticmethod
+    def remove(assignment_group_id):
+        targ_group = AssignmentGroup.objects.get(assignment_group_id)
+        AssignmentGroup.objects.get(pk=assignment_group_id).delete()
+        AssignmentGroupMembership.query.filter_by(assignment_group=targ_group).delete()
+
+    @staticmethod
+    def edit(assignment_group_id, name=None):
+        assignment_group = AssignmentGroup.objects.get(pk=assignment_group_id)
+        if name is not None:
+            assignment_group.name = name
+        assignment_group.save()
+        return assignment_group
+
+    @staticmethod
+    def by_id(assignment_group_id):
+        return AssignmentGroup.objects.get(pk=assignment_group_id)
+
+    @staticmethod
+    def by_course(course_id):
+        course = Course.objects.get(pk=course_id)
+        return (AssignmentGroup.objects.filter(course=course)
+                                     .order_by(AssignmentGroup.name)
+                                     .all())
+
+    @staticmethod
+    def get_ungrouped_assignments(course_id):
+        course = Course.objects.get(pk=course_id)
+        return (Assignment.objects
+                          .filter(course=course)
+                          .outerjoin(AssignmentGroupMembership)
+                          .filter(AssignmentGroupMembership.assignment_id==None)
+                          .all())
+
+    def get_assignments(self):
+        return (Assignment.query
+                          .join(AssignmentGroupMembership,
+                                AssignmentGroupMembership.assignment_id == Assignment.id)
+                          .filter(AssignmentGroupMembership.assignment_group_id==self.id)
+                          .order_by(AssignmentGroupMembership.position)
+                          .all())
+
+
+class AssignmentGroupMembership(Base):
+    assignment_group = models.ForeignKey(AssignmentGroup, on_delete=models.CASCADE)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    position = models.IntegerField()
+
+    @staticmethod
+    def move_assignment(assignment_id, new_group_id):
+        assignment = Assignment.objects.get(pk=assignment_id)
+        new_assignment_group = AssignmentGroup.objects.get(pk=new_group_id)
+        membership = (AssignmentGroupMembership.objects
+                                               .filter(assignment=assignment)
+                                               .first())
+        if membership is None:
+            membership = AssignmentGroupMembership(assignment_group=new_assignment_group,
+                                                   assignment=assignment,
+                                                   position=0)
+        else:
+            membership.assignment_group = new_assignment_group
+        membership.save()
+        return membership
+
+
+class Submission(Base):
+    code = models.TextField(default="")
+    status = models.IntegerField(default=0)
+    correct = models.BooleanField(default=False)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    user= models.ForeignKey(LTIUser, on_delete=models.CASCADE)
+    assignment_version = models.IntegerField(default=0)
+    version = models.IntegerField(default=0)
 
     def __str__(self):
         return '<Submission {} for {}>'.format(self.id, self.user)
@@ -216,204 +429,11 @@ class Submission(models.Model):
         )
 
 
-class Assignment(Base):
-    url = CharField(max_length=255, default="")
-    name = CharField(max_length=255, default="Untitled")
-    body = TextField(default="")
-    on_run = TextField(default="def on_run(code, output, properties):\n    return True")
-    on_step = TextField(default="def on_step(code, output, properties):\n    return True")
-    on_start = TextField(default="")
-    answer = TextField(default="")
-    due = DateTimeField(default=None)
-    type = CharField(max_length=10, default="normal")
-    visibility = CharField(max_lenght=10, default="visible")
-    disabled = CharField(max_length=10, default="enabled")
-    mode = CharField(max_length=10, default="blocks")
-    owner = ForeignKey(LTIUser, on_delete=models.CASCADE)
-    course = ForeignKey(Course, on_delete=models.CASCADE)
-    version = IntegerField(default=0)
-
-    @staticmethod
-    def edit(assignment_id, presentation=None, name=None, on_run=None, on_step=None, on_start=None, parsons=None, text_first=None):
-        assignment = Assignment.objects.get(pk=assignment_id)
-        if name is not None:
-            assignment.name = name
-            assignment.version += 1
-        if presentation is not None:
-            assignment.body = presentation
-            assignment.version += 1
-        if on_run is not None:
-            assignment.on_run = on_run
-            assignment.version += 1
-        if on_step is not None:
-            assignment.on_step = on_step
-            assignment.version += 1
-        if on_start is not None:
-            assignment.on_start = on_start
-            assignment.version += 1
-        assignment.type = 'normal'
-        if parsons is True:
-            assignment.type = 'parsons'
-            assignment.version += 1
-        if text_first is True:
-            assignment.type = 'text'
-            assignment.version += 1
-        assignement.save()
-        return assignment
-
-    def to_dict(self):
-        return {
-            'name': self.name,
-            'id': self.id,
-            'body': self.body,
-            'title': self.title()
-        }
-
-    def __str__(self):
-        return '<Assignment {} for {}>'.format(self.id, self.course.id)
-
-    def title(self):
-        return self.name if self.name != "Untitled" else "Untitled ({})".format(self.id)
-
-    @staticmethod
-    def new(owner_id, course_id):
-        owner = LTIUser.objects.get(pk=owner_id)
-        course = Course.objects.get(pk=course_id)
-        assignment = Assignment(owner=owner, course=course)
-        assignement.save()
-        return assignment
-
-    @staticmethod
-    def remove(assignment_id):
-        Assignment.objects.get(pk=assignment_id).delete()
-
-    @staticmethod
-    def by_course(course_id, exclude_builtins=True):
-        if exclude_builtins:
-            course = Course.objects.get(pk=course_id)
-            return (Assignment.objects.filter(course=course)
-						                        .exclude(mode=maze)
-                                    .all())
-        else:
-            return Assignment.objects.filter(course=course)
-
-    @staticmethod
-    def by_id(assignment_id):
-        return Assignment.objects.get(pk=assignment_id)
-
-    @staticmethod
-    def by_builtin(type, id, owner_id, course_id):
-        course = Course.object.get(pk=course_id)
-        assignment = Assignment.objects.filter(course=course,
-                                                mode=type,
-                                                name=id).first()
-        if not assignment:
-            assignment = Assignment.new(owner_id, course_id)
-            assignment.mode = type
-            assignment.name = id
-            assignment.save()
-        return assignment
-
-    @staticmethod
-    def by_id_or_new(assignment_id, owner_id, course_id):
-        if assignment_id is None:
-            assignment = None
-        else:
-            assignment = Assignment.objects.get(pk=assignment_id)
-        if not assignment:
-            assignment = Assignment.new(owner_id, course_id)
-        return assignment
-
-    def context_is_valid(self, context_id):
-        course = Course.objects.get(pk=self.course_id)
-        if course:
-            return course.external_id == context_id
-        return False
-
-    def get_submission(self, user_id):
-        return Submission.load(user_id, self.id)
-
-class AssignmentGroup(Base):
-    name = CharField(max_length=255, default="Untitled")
-    owner = ForeignKey(LTIUser, on_delete=models.CASCADE)
-    course = ForeignKey(Course, on_delete=models.CASCADE)
-
-    @staticmethod
-    def new(owner_id, course_id):
-        owner = LTIUser.objects.get(pk=owner_id)
-        course = Course.objects.get(pk=course_id)
-        assignment_group = AssignmentGroup(owner=owner, course=course)
-        assignment_group.save()
-        return assignment_group
-
-    @staticmethod
-    def remove(assignment_group_id):
-        targ_group = AssignmentGroup.objects.get(assignment_group_id)
-        AssignmentGroup.objects.get(pk=assignment_group_id).delete()
-        AssignmentGroupMembership.query.filter_by(assignment_group=targ_group).delete()
-
-    @staticmethod
-    def edit(assignment_group_id, name=None):
-        assignment_group = AssignmentGroup.objects.get(pk=assignment_group_id)
-        if name is not None:
-            assignment_group.name = name
-        assignment_group.save()
-        return assignment_group
-
-    @staticmethod
-    def by_id(assignment_group_id):
-        return AssignmentGroup.objects.get(pk=assignment_group_id)
-
-    @staticmethod
-    def by_course(course_id):
-        course = Course.objects.get(pk=course_id)
-        return (AssignmentGroup.objects.filter(course=course)
-                                     .order_by(AssignmentGroup.name)
-                                     .all())
-
-    @staticmethod
-    def get_ungrouped_assignments(course_id):
-        course = Course.objects.get(pk=course_id)
-        return (Assignment.objects
-                          .filter(course=course)
-                          .outerjoin(AssignmentGroupMembership)
-                          .filter(AssignmentGroupMembership.assignment_id==None)
-                          .all())
-
-    def get_assignments(self):
-        return (Assignment.query
-                          .join(AssignmentGroupMembership,
-                                AssignmentGroupMembership.assignment_id == Assignment.id)
-                          .filter(AssignmentGroupMembership.assignment_group_id==self.id)
-                          .order_by(AssignmentGroupMembership.position)
-                          .all())
-
-class AssignmentGroupMembership(Base):
-    assignment_group = ForeignKey(AssignmentGroup, on_delete=models.CASCADE)
-    assignment = ForeignKey(Assignement, on_delete=models.CASCADE)
-    position = IntegerField()
-
-    @staticmethod
-    def move_assignment(assignment_id, new_group_id):
-        assignment = Assignment.objects.get(pk=assignment_id)
-        new_assignment_group = AssignmentGroup.objects.get(pk=new_group_id)
-        membership = (AssignmentGroupMembership.objects
-                                               .filter(assignment=assignment)
-                                               .first())
-        if membership is None:
-            membership = AssignmentGroupMembership(assignment_group=new_assignment_group,
-                                                   assignment=assignment,
-                                                   position=0)
-        else:
-            membership.assignment_group = new_assignment_group
-        membership.save()
-        return membership
-
 class Log(Base):
-    event = CharField(max_length=255, default="")
-    action = CharField(max_length=255, default="")
-    assignment = ForeignKey(Assignment, on_delete=models.CASCADE)
-    user = ForeignKey(LTIUser, on_delete=models.CASCADE)
+    event = models.CharField(max_length=255, default="")
+    action = models.CharField(max_length=255, default="")
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    user = models.ForeignKey(LTIUser, on_delete=models.CASCADE)
 
     @staticmethod
     def new(event, action, assignment_id, user_id):
