dd95aaaba20127b03231261feea018337b4a9cd3
groovy-tunes <chinsenjeremy@gmail.com>
2017-07-13 14:42:20 -0400

    Added models to lti_django_skeleton/models.py

--- lti_django_skeleton/admin.py
+++ lti_django_skeleton/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import LTIUser, Course, Role, Authentication, Settings,
+                    Submission, Assignment, AssignmentGroup,
+                    AssignmentGroupMembership, Log
+
+admin.site.register(LTIUser)
+admin.site.register(Course)
+admin.site.register(Role)
+admin.site.register(Authentication)
+admin.site.register(Settings)
+admin.site.register(Submission)
+admin.site.register(Assignment)
+admin.site.register(AssignmentGroup)
+admin.site.register(AssignmentGroupMembership)
+admin.site.register(Log)
