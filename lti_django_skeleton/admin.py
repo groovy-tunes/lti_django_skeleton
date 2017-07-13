18789355ae6db8939863019b81a98e6c0ed351f3
Anthony Tam <anthony@anthonytam.me>
2017-07-13 15:19:48 -0400

    Fixed admin file

--- lti_django_skeleton/admin.py
+++ lti_django_skeleton/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 
-from .models import LTIUser, Course, Role, Authentication, Settings,
-                    Submission, Assignment, AssignmentGroup,
+from .models import Course, Role, Settings, \
+                    Submission, Assignment, AssignmentGroup, \
                     AssignmentGroupMembership, Log
 
-admin.site.register(LTIUser)
 admin.site.register(Course)
 admin.site.register(Role)
-admin.site.register(Authentication)
 admin.site.register(Settings)
 admin.site.register(Submission)
 admin.site.register(Assignment)
